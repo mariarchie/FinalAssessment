2a9cc3c765e12cb6d45b28483408bd2999bdeff9
Marianna <mariannadek@gmail.com>
2023-01-20 21:21:58 +0400

    Menu and Main are changed

--- Java/Main.java
+++ Java/Main.java
@@ -1,30 +1,14 @@
 package Java;
 
-import java.io.FileInputStream;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
-
 public class Main {
-    static Logger LOGGER;
-    static {
-        try(FileInputStream ins = new FileInputStream("/Users/mariannaadekova/Desktop/GB/Attestation")){ 
-            LogManager.getLogManager().readConfiguration(ins);
-            LOGGER = Logger.getLogger(Main.class.getName());
-        }catch (Exception ignore){
-            ignore.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) {
-
         try {
             while (true) {
                 Menu menu = new Menu();
                 menu.start();
             }
-
         } catch (Exception e) {
-            System.out.println("Come back soon!");
+            System.out.println("Come back soon");
         }
     }
 }
--- Java/Menu.java
+++ Java/Menu.java
@@ -6,6 +6,7 @@ import Java.Beasts.Beast;
 import Java.Pets.Pet;
 
 public class Menu {
+
     static void showMainMenu() {
         System.out.println(
                 "Welcome to the Pets and Beasts Cattery" + "\n" +
@@ -17,20 +18,19 @@ public class Menu {
     }
 
     void start() {
-
         boolean end = false;
-
         while (!end) {
             showMainMenu();
+
             Scanner sc = new Scanner(System.in);
             int input = sc.nextInt();
             if (input == 1) {
                 System.out.println(
                         "Введите тип животного:" + "\n"
                                 + "1. Домашние" + "\n"
-                                + "2. Вьючные" + "\n");
-                int chooseType = sc.nextInt();
-                if (chooseType == 1) {
+                                + "2. Вьючные");
+                int type = sc.nextInt();
+                if (type == 1) {
                     System.out.println("Введите id животного: ");
                     int id = sc.nextInt();
                     System.out.println("Введите имя животного: ");
@@ -42,7 +42,7 @@ public class Menu {
                     Pet aPet = new Pet(id, name, birthdate, thoroughbred);
                     System.out.println(aPet);
                 }
-                if (chooseType == 2) {
+                if (type == 2) {
                     System.out.println("Введите id животного: ");
                     int id = sc.nextInt();
                     System.out.println("Введите имя животного:");
@@ -54,67 +54,56 @@ public class Menu {
                     Beast aBeast = new Beast(id, name, birthdate, loadCapacity);
                     System.out.println(aBeast);
                 }
+            }
 
-            } else if (input == 2) {
+            if (input == 2) {
                 System.out.println("Команды какого животного Вы желаете посмотреть?(Введите id)");
-                int id = sc.nextInt();
-                System.out.println(
-                        "Выберите тип животного:" + "\n"
-                                + "1. Домашние" + "\n"
-                                + "2. Вьючные" + "\n");
-                int chooseType = sc.nextInt();
-                if (chooseType == 1) {
-                    PetStorage storage = new PetStorage();
-                    for (int i = 0; i < storage.pets.size(); i++) {
-                        if (storage.pets.get(i).getId() == id) {
-                            storage.pets.get(i).showCommands(storage.pets.get(i).commands);
-                        }
+                int animalId = sc.nextInt();
+                PetStorage pstorage = new PetStorage();
+                for (int i = 0; i < pstorage.pets.size(); i++) {
+                    if (pstorage.pets.get(i).getId() == animalId) {
+                        pstorage.pets.get(i).showCommands(pstorage.pets.get(i).commands);
                     }
                 }
-                if (chooseType == 2) {
-                    BeastStorage storage = new BeastStorage();
-                    for (int i = 0; i < storage.beasts.size(); i++) {
-                        if (storage.beasts.get(i).getId() == id) {
-                            storage.beasts.get(i).showCommands(storage.beasts.get(i).commands);
-                        }
+                BeastStorage bstorage = new BeastStorage();
+                for (int i = 0; i < bstorage.beasts.size(); i++) {
+                    if (bstorage.beasts.get(i).getId() == animalId) {
+                        bstorage.beasts.get(i).showCommands(bstorage.beasts.get(i).commands);
                     }
                 }
+            }
 
-            } else if (input == 3) {
-                System.out.println("Введите id животного, которого будем обучать команде: ");
+            if (input == 3) {
+                System.out.println("Какого животного будем обучать?(Введите id) ");
                 int animalID = sc.nextInt();
 
                 System.out.println("Какой команде будем обучать животное? ");
                 String inputCommand = sc.next();
-                System.out.println(
-                        "Выберите тип животного:" + "\n"
-                                + "1. Домашние" + "\n"
-                                + "2. Вьючные" + "\n");
-                int animalType = sc.nextInt();
-                if (animalType == 1) {
-                    PetStorage storage = new PetStorage();
-                    for (int i = 0; i < storage.pets.size(); i++) {
-                        if (storage.pets.get(i).getId() == animalID) {
-                            storage.pets.get(i).addCommand(inputCommand, ((Animal) (storage.pets.get(i))));
-                        }
-                    }
-                }
-                if (animalType == 2) {
-                    BeastStorage storage = new BeastStorage();
-                    for (int i = 0; i < storage.beasts.size(); i++) {
-                        if (storage.beasts.get(i).getId() == animalID) {
-                            storage.beasts.get(i).addCommand(inputCommand, ((Animal) (storage.beasts.get(i))));
-                        }
+
+                PetStorage ptstorage = new PetStorage();
+                for (int i = 0; i < ptstorage.pets.size(); i++) {
+                    if (ptstorage.pets.get(i).getId() == animalID) {
+                        ptstorage.pets.get(i).addCommand(inputCommand, ((Animal) (ptstorage.pets.get(i))));
                     }
                 }
 
-            } else if (input == 4) {
+                BeastStorage bststorage = new BeastStorage();
+                for (int i = 0; i < bststorage.beasts.size(); i++) {
+                    if (bststorage.beasts.get(i).getId() == animalID) {
+                        bststorage.beasts.get(i).addCommand(inputCommand, ((Animal) (bststorage.beasts.get(i))));
+                    }
+                }
+            }
+
+            if (input == 4) {
                 end = true;
                 sc.close();
+
             } else {
-                end = true;
+                System.out.println("Wrong number");
                 sc.close();
             }
+
         }
     }
 }
